4f08a741d725f8484392a50d8a42aefe2440d98d
Alex Shook <alexandra.shook@gmail.com>
2013-12-10 22:35:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Retourist::Application.config.secret_key_base = '6294cf4d17fb0bc838c15c959cb4677b5243b6af1745dfa70cc1426bd15ffa2842405bacd18f5ac4cf498a5137e29fbf1d171b3b14c0443a5a15c1a9df5a00a6'
