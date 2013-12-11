03cbd226480bac452b6411838a5eb9364f105af7
Alex Shook <alexandra.shook@gmail.com>
2013-12-11 00:09:44 -0500

    fixing search

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,6 @@
 class City < ActiveRecord::Base
 	validates :name, presence: true
 	def self.search_for(query)
-		self.where('title LIKE :query OR category LIKE :query OR link LIKE :query', query: "%#{query}%")
+		self.where('name LIKE :query', query: "%#{query}%")
 	end
 end
