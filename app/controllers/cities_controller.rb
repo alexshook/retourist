c141901c9f3e576193cb8c07c20ba0060a8176d4
Alex Shook <alexandra.shook@gmail.com>
2013-12-13 18:21:47 -0500

    added admin model, create and edit functionality

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,4 +3,21 @@ class CitiesController < ApplicationController
  	def index
   		@cities = City.all
   	end
+
+  	def new
+  		@city = City.create
+  	end
+
+  	def create
+  		safe_city = params.require(:city).permit(:name, :rating)
+  		city = City.create safe_city
+  		redirect_to city
+  	end
+
+  	def update
+  		@city = City.find params[:id]
+   		safe_city = params.require(:city).permit(:name, :rating)
+  		city = City.create safe_city
+  		redirect_to city
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Retourist::Application.routes.draw do
+  devise_for :admins
+  get "cities/index"
+  get "users/index"
   get "search", to: "search#index"
   root "cities#index"
-  resources :cities
+  resources :cities, only: [:index, :new, :create, :show, :edit, :update]
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
