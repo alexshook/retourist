e96a75397c52b324759b3d7150951818c1dda7ed
Alex Shook <alexandra.shook@gmail.com>
2014-01-08 20:14:15 -0500

    fixing paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ActiveRecord::Base
 	has_many :messages_sent, class_name: "User", primary_key: "user_id", foreign_key: "sender_id"
 	has_many :messages_received, class_name: "User", primary_key: "user_id", foreign_key: "recipient_id"
 	has_many :recipients, through: :messages_sent
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :path => ":rails_root/public/system/:attachment/:id/:style/:filename", :url => "/system/:attachment/:id/:style/:filename"
+	has_attached_file :avatar, :styles => { :medium => "300x300>", 
+		:thumb => "100x100>" }, 
+		:default_url => "/images/:style/missing.png", 
+		:path => ":rails_root/public/system/:attachment/:id/:style/:filename", 
+		:url => "/system/:attachment/:id/:style/:filename"
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
