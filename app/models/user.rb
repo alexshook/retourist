44af8d3337f4fce368a1371fb9d4d449fe854677
Alex Shook <alexandra.shook@gmail.com>
2014-01-11 15:35:02 -0500

    trying something else for paperclip s3

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ActiveRecord::Base
 	has_attached_file :avatar, :styles => 
 		{ :medium => "300x300>", 
 			:thumb => "100x100>" }, 
-		:default_url => "/images/:style/missing.png"
-		# :path => ":rails_root/public/system/:attachment/:id/:style/:filename", 
-		# :url => "/system/:attachment/:id/:style/:filename"
+		:default_url => "/images/:style/missing.png",
+		:path => ":rails_root/public/system/:attachment/:id/:style/:filename", 
+		:url => "/system/:attachment/:id/:style/:filename"
 
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
