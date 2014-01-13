05d18abe362f530bff43b8ea6f377478ad1e449f
Alex Shook <alexandra.shook@gmail.com>
2014-01-13 00:41:38 -0500

    index styling, pages styling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,12 @@
 $(document).on('ready page:load', function() {
   $('#backstretch').backstretch([
   	"http://farm4.staticflickr.com/3792/11821175215_c4d71fce3b_h.jpg", 
-  	"http://farm3.staticflickr.com/2389/2311968821_02406536b7_o.jpg", 
-  	"http://farm4.staticflickr.com/3645/3615847616_2f61b2e36b_o.jpg"
+  	"http://farm3.staticflickr.com/2836/11898803116_d4257fb77e_o.jpg",
+  	"http://farm4.staticflickr.com/3645/3615847616_2f61b2e36b_o.jpg", 
+  	"http://farm3.staticflickr.com/2330/2323976052_f100fa98b5_o.jpg",
+  	"http://farm6.staticflickr.com/5285/5284971534_37b56c4eea_o.jpg", 
+  	"http://farm4.staticflickr.com/3383/3568368637_4e6e6c6637_o.jpg",
+  	"http://farm6.staticflickr.com/5093/5536740980_bfee867294_o.jpg",
+  	"http://farm6.staticflickr.com/5282/5284366751_b072bbb482_o.jpg"
   	], {duration: 5000, fade: 750});
 });
